028c169e48138bc9bef2f023ae36895b93a803a8
Timothy D <ttimm5050@gmail.com>
2026-04-20 13:13:05 +0100

    Add files via upload

---
+++ result-bridge/result-bridge/src/main/java/com/max/result_bridge/ResultBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.max.result_bridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResultBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResultBridgeApplication.class, args);
+	}
+
+}
---
+++ result-bridge/result-bridge/src/test/java/com/max/result_bridge/ResultBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.max.result_bridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResultBridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
